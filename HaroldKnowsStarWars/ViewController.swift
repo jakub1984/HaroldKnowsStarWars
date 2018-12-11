4f5795c5587e5b60c0ff8b65fe13bc6ab344642a
Jakub <j.perich@seznam.cz>
2018-12-11 15:38:30 +0700

    Initial Commit

---
+++ HaroldKnowsStarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HaroldKnowsStarWars
+//
+//  Created by Jakub Perich on 11/12/2018.
+//  Copyright © 2018 Jakub Perich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
